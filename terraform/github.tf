3f51532e146108aa6fae222cafbe77bca19314c2
Alexander Proschek <alexander.proschek@protonmail.com>
2023-07-17 18:27:39 +0700

    feat: Add some more repo configuration

--- terraform/github.tf
+++ terraform/github.tf
@@ -11,6 +11,8 @@ resource "github_repository" "h2odrinkingwater" {
   name       = "h2odrinkingwater"
   visibility = "public"
 
+  homepage_url = "https://www.h2odrinkingwater.com/"
+
   pages {
     source {
       branch = "gh-pages"
@@ -18,4 +20,14 @@ resource "github_repository" "h2odrinkingwater" {
     }
     cname = "www.h2odrinkingwater.com"
   }
+}
+
+resource "github_branch" "main" {
+  repository = github_repository.h2odrinkingwater.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "default"{
+  repository = github_repository.h2odrinkingwater.name
+  branch     = github_branch.main.branch
 }
